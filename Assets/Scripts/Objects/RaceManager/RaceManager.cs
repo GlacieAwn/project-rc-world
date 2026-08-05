56ef3684837006e7f551a82a1a49ae36373e8bc4
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-08-05 19:40:53 -0400

    Implement timer logi

--- Assets/Scripts/Objects/RaceManager/RaceManager.cs
+++ Assets/Scripts/Objects/RaceManager/RaceManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RaceManager : MonoBehaviour
@@ -12,6 +14,7 @@ public class RaceManager : MonoBehaviour
 
     [SerializeField] private RaceState raceState;
     [SerializeField] private TMP_Text debugText;
+    [SerializeField] private float currentTime = 0f;
 
 
     public enum RaceState
@@ -28,24 +31,39 @@ public class RaceManager : MonoBehaviour
     public Action OnRaceFinished;
     public Action<RaceState, RaceState> OnRaceStateChanged;
 
+    private TimeSpan time;
+
+    void Start()
+    {
+        SetState(RaceState.Countdown);
+    }
 
 	void Awake()
 	{
-		raceState = RaceState.Waiting;
+		OnRaceStateChanged += HandleRaceStateChanged;
 	}
 
     void Update()
     {
+        time = TimeSpan.FromSeconds(currentTime);
+        
+
+        if(raceState == RaceState.Racing)
+        {
+            currentTime += Time.deltaTime;
+        }
+
         UpdateDebugText();
     }
 
     private void BeginCountdown()
     {
-        
+        StartCoroutine(countdown());
     }
 
     private bool StartRace()
-    {   
+    {
+        currentTime = 0f;
         return true;
     }
 
@@ -54,15 +72,48 @@ public class RaceManager : MonoBehaviour
         return true;
     }
 
-    private bool SetState(RaceState newState)
-    {   
-        return true;
+    private void SetState(RaceState newState)
+    {
+        if (raceState == newState)
+            return;
+
+        RaceState previous = raceState;
+        raceState = newState;
+
+        OnRaceStateChanged?.Invoke(previous, newState);
+    }
+
+    private void HandleRaceStateChanged(RaceState previous, RaceState current)
+    {
+        switch (current)
+        {
+            case RaceState.Countdown:
+                BeginCountdown();
+                break;
+
+            case RaceState.Racing:
+                StartRace();
+                break;
+
+            case RaceState.Finished:
+                FinishRace();
+                break;
+        }
     }
 
     private void UpdateDebugText()
     {
+        int hundredths = time.Milliseconds / 10;
+
         debugText.text =
         "Race Manager Values:\n" +
-        $"currentState: {raceState}";
+        $"currentState: {raceState}\n" +
+        $"currentTime:{time.Minutes:00}:{time.Seconds:00}.{hundredths:00}";
+    }
+
+    IEnumerator countdown()
+    {
+        yield return new WaitForSeconds(3f);
+        SetState(RaceState.Racing);
     }
 }
