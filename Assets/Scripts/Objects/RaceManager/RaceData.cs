f8b3d850835d0685fc1c04add013ff01af7f52e3
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-08-06 14:38:02 -0400

    Create and populate RaceData class

---
+++ Assets/Scripts/Objects/RaceManager/RaceData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RaceData
+{
+	public int CurrentLap;
+    public int CurrentCheckpoint;
+    public int CurrentPlace;
+
+    public float CurrentTime;
+
+    public bool Finished;
+}
